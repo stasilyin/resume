37abd849f910e33452d42fa20bb6ff882627d80e
stasilyin <stasilyin.by@gmail.com>
2021-01-04 02:42:20 +0300

    add star rating

--- js/script.js
+++ js/script.js
@@ -1,64 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(event) {
-let rating = document.querySelector('.skills-rating');
- let  ratingItem = document.querySelectorAll('.star');
 
-rating.onclick = function(e){
-  let target = e.target;
- 
-  if(target.classList.contains('star')){
-    removeClass(ratingItem,'current-active')
-    target.classList.add('active','current-active');
-  }
-}
+const ratingItemsList = document.querySelectorAll('.skills-rating-item');
+const ratingItemsArray = Array.prototype.slice.call(ratingItemsList);
 
-rating.onmouseover = function(e) {
-    let target = e.target;
-  if(target.classList.contains('star')){
-    removeClass(ratingItem,'active')
-    target.classList.add('active');
-    mouseOverActiveClass(ratingItem)
-  }
-}
-rating.onmouseout = function(){
-  addClass(ratingItem,'active');
-  mouseOutActiveClas(ratingItem);
-}
+ratingItemsArray.forEach (item =>
+  item.addEventListener('click', () => {
+    const {itemValue} = item.dataset;
+  item.parentNode.dataset.totalValue = itemValue;
 
-function removeClass(arr) {
-  for(let i = 0, iLen = arr.length; i <iLen; i ++) {
-    for(let j = 1; j < arguments.length; j ++) {
-      ratingItem[i].classList.remove(arguments[j]);
-    }
-  }
-}
-function addClass(arr) {
-  for(let i = 0, iLen = arr.length; i <iLen; i ++) {
-    for(let j = 1; j < arguments.length; j ++) {
-      ratingItem[i].classList.add(arguments[j]);
-    }
-  }
-}
-
-function mouseOverActiveClass(arr){
-  for(let i = 0, iLen = arr.length; i < iLen; i++) {
-    if(arr[i].classList.contains('active')){
-      break;
-    }else {
-      arr[i].classList.add('active');
-    }
-  }
-}
-
-function mouseOutActiveClas(arr){
-  for(let i = arr.length-1; i >=1; i--) {
-    if(arr[i].classList.contains('current-active')){
-      break;
-    }else {
-      arr[i].classList.remove('active');
-    }
-  }
-}
-
+  //request
+})
+);
 $('.skills-btn-wrapper').on('click', function(e){
     e.preventDefault;
     $(this).toggleClass('skills-btn-active');
