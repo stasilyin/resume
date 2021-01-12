154cdd97ecdd686f7feb5334a45673213e34e008
stasilyin <stasilyin.by@gmail.com>
2021-01-12 13:47:28 +0300

    change script from jquery to js, change slider

--- js/script.js
+++ js/script.js
@@ -11,17 +11,42 @@ ratingItemsArray.forEach (item =>
   //request
 })
 );
-$('.skills-btn-wrapper').on('click', function(e){
-    e.preventDefault;
+
+/* $('.skills-btn-wrapper').on('click', function(e){
+    e.preventDefault();
     $(this).toggleClass('skills-btn-active');
     $('.skills-wrapper').toggleClass('skills-wrapper-active');
+}); */
+
+/*Slider initialization*/
+new Swiper('.swiper-container', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  grabCursor: true,
+  keyboard: {
+    enable:true,
+    pageUpDown:true,
+  },
+  mousewheel: {
+    sensitivity:1,
+    eventsTarget:".project-slider",
+  }
 });
-$(document).ready(function(){
-$('.project').slick({
-  arrows:false,
-  dots: true,
-  easing: 'ease'
-  
 });
-});
+
+document.querySelector('.skills-btn-wrapper').addEventListener('click',function(e){
+  e.preventDefault();
+  let currentHeight = document.querySelector('.skills-wrapper').offsetHeight;
+  console.log(currentHeight);
+  if (currentHeight == 213) {
+    this.classList.add('skills-btn-active');
+    document.querySelector('.skills-wrapper').classList.add('skills-wrapper-active');
+  }
+  else {
+    this.classList.remove ('skills-btn-active');
+    document.querySelector('.skills-wrapper').classList.remove('skills-wrapper-active');
+  }
+
 });
