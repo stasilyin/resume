4657646a6b9d410ee59a4b97128b365dec6b247c
stasilyin <stasilyin.by@gmail.com>
2021-01-13 11:10:48 +0300

    removed unnecessary

--- js/script.js
+++ js/script.js
@@ -1,52 +1,42 @@
-document.addEventListener('DOMContentLoaded', function(event) {
-
-const ratingItemsList = document.querySelectorAll('.skills-rating-item');
+const ratingItemsList = document.querySelectorAll(".skills-rating-item");
 const ratingItemsArray = Array.prototype.slice.call(ratingItemsList);
 
-ratingItemsArray.forEach (item =>
-  item.addEventListener('click', () => {
-    const {itemValue} = item.dataset;
-  item.parentNode.dataset.totalValue = itemValue;
+ratingItemsArray.forEach((item) =>
+  item.addEventListener("click", () => {
+    const {
+      itemValue
+    } = item.dataset;
+    item.parentNode.dataset.totalValue = itemValue;
 
-  //request
-})
+    //request
+  })
 );
 
-/* $('.skills-btn-wrapper').on('click', function(e){
-    e.preventDefault();
-    $(this).toggleClass('skills-btn-active');
-    $('.skills-wrapper').toggleClass('skills-wrapper-active');
-}); */
-
 /*Slider initialization*/
-new Swiper('.swiper-container', {
+
+new Swiper(".swiper-container", {
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
   },
   grabCursor: true,
   keyboard: {
-    enable:true,
-    pageUpDown:true,
+    enable: true,
+    pageUpDown: true,
   },
   mousewheel: {
-    sensitivity:1,
-    eventsTarget:".project-slider",
-  }
-});
+    sensitivity: 1,
+    eventsTarget: ".project-slider",
+  },
 });
 
-document.querySelector('.skills-btn-wrapper').addEventListener('click',function(e){
+/*Block skills*/
+const skillsBtnWrapper = document.querySelector(".skills-btn-wrapper");
+const skillsWrapper = document.querySelector(".skills-wrapper");
+
+const toggleBtnWrapper = (e) => {
   e.preventDefault();
-  let currentHeight = document.querySelector('.skills-wrapper').offsetHeight;
-  console.log(currentHeight);
-  if (currentHeight == 213) {
-    this.classList.add('skills-btn-active');
-    document.querySelector('.skills-wrapper').classList.add('skills-wrapper-active');
-  }
-  else {
-    this.classList.remove ('skills-btn-active');
-    document.querySelector('.skills-wrapper').classList.remove('skills-wrapper-active');
-  }
-
-});
+  skillsBtnWrapper.classList.toggle("skills-btn-active");
+  skillsWrapper.classList.toggle("skills-wrapper-active");
+};
+skillsBtnWrapper.addEventListener("click", toggleBtnWrapper);
