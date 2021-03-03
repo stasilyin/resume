8c6bebee550153f1d103b3ed19e8ec87a5374c2d
Stas Ilyin <stasilyin.by@gmail.com>
2021-03-04 02:21:00 +0300

    add mobile version

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* Star animation */
 const ratingItemsList = document.querySelectorAll(".skills-rating-item");
 const ratingItemsArray = Array.prototype.slice.call(ratingItemsList);
 
@@ -7,12 +8,12 @@ ratingItemsArray.forEach((item) =>
       itemValue
     } = item.dataset;
     item.parentNode.dataset.totalValue = itemValue;
-
-    //request
+    console.log(item.dataset);
+    // request
   })
 );
 
-/*Slider initialization*/
+/* Slider initialization */
 
 new Swiper(".swiper-container", {
   pagination: {
@@ -28,15 +29,39 @@ new Swiper(".swiper-container", {
     sensitivity: 1,
     eventsTarget: ".project-slider",
   },
+  autoHeight: true,
 });
 
-/*Block skills*/
+/* Block skills */
 const skillsBtnWrapper = document.querySelector(".skills-btn-wrapper");
 const skillsWrapper = document.querySelector(".skills-wrapper");
 
 const toggleBtnWrapper = (e) => {
   e.preventDefault();
-  skillsBtnWrapper.classList.toggle("skills-btn-active");
-  skillsWrapper.classList.toggle("skills-wrapper-active");
+  console.log();
+  if (skillsWrapper.offsetHeight === 440 || skillsWrapper.offsetHeight === 2390) {
+    skillsBtnWrapper.classList.toggle("skills-btn-active");
+    skillsWrapper.classList.toggle("skills-wrapper-mobile-active");
+  } else {
+    skillsBtnWrapper.classList.toggle("skills-btn-active");
+    skillsWrapper.classList.toggle("skills-wrapper-active");
+  }
 };
 skillsBtnWrapper.addEventListener("click", toggleBtnWrapper);
+const tripHouse = document.querySelector('.tripHouse');
+const resume = document.querySelector('.resume');
+const mobileScreen = window.matchMedia('(max-width: 768px)');
+const changeText = (e) => {
+  if (e.matches) {
+    tripHouse.textContent = 'Triphouse – Github';
+    resume.textContent = 'Landing CV – Github';
+  } else {
+    tripHouse.textContent = 'Seen on GitHub';
+    resume.textContent = 'Seen on GitHub';
+  }
+}
+if (mobileScreen.matches) {
+  tripHouse.textContent = 'Triphouse – Github';
+  resume.textContent = 'Landing CV – Github';
+} 
+mobileScreen.addEventListener('change', changeText);
